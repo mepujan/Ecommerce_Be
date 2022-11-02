f9830bec171eba81432099c32279b147be92a51e
Pujan Gautam <mepujan10@gmail.com>
2022-11-02 18:29:36 -0400

    mend

--- model/user.js
+++ model/user.js
@@ -1,61 +1,65 @@
-import mongoose, { Schema } from "mongoose";
-import { isEmail } from validator;
+import mongoose, {
+    Schema
+} from "mongoose";
+import {
+    isEmail
+} from validator;
 
 const UserSchema = new Schema({
-    first_name:{
-        type:String,
-        required:true,
+    first_name: {
+        type: String,
+        required: true,
     },
     middle_name: String,
-    last_name:{
-        type:String,
-        required:true
+    last_name: {
+        type: String,
+        required: true
     },
-    username:{
-        type:String,
-        unique:true,
-        required:true
+    username: {
+        type: String,
+        unique: true,
+        required: true
     },
-    email:{
-        type:String,
-        lowercase:true,
-        unique:true,
-        required:true,
-        validate:[isEmail,"Invalid Email Address."]
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: true,
+        validate: [isEmail, "Invalid Email Address."]
     },
-    password:{
-        type:String,
-        required:false,
+    password: {
+        type: String,
+        required: false,
         select: false
     },
-    street_name:{
-        type:String,
-        required:true
+    street_name: {
+        type: String,
+        required: true
     },
-    postal_code:{
-        type:String,
-        required:true
+    postal_code: {
+        type: String,
+        required: true
     },
-    city_name:{
-        type:String,
-        required:true
+    city_name: {
+        type: String,
+        required: true
     },
-    state:{
-        type:String,
-        required:true
+    state: {
+        type: String,
+        required: true
     },
-    country:{
-        type:String,
-        required:true
+    country: {
+        type: String,
+        required: true
     },
-    dob:{
-        type:Date,
-        required:true
+    dob: {
+        type: Date,
+        required: true
     },
-    profile_picture :{
-        type:Buffer,
-        contentType:String
+    profile_picture: {
+        type: Buffer,
+        contentType: String
     }
 });
 
-module.export = mongoose.model('User',UserSchema);
+module.export = mongoose.model('User', UserSchema);
