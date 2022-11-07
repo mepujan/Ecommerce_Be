59673d0b85e2e444a6e304a2bd7cd8776c44a3d6
Pujan Gautam <mepujan10@gmail.com>
2022-11-07 14:38:01 -0500

    [updated] added put, delete, get item by id api routes

--- routers/vendors.js
+++ routers/vendors.js
@@ -6,6 +6,10 @@ import {validate} from 'express-validation';
 const vendorRoutes = express.Router();
 const vendorController = new VendorController();
 
-vendorRoutes .post("/vendor/create",validate(VendorValidations.CreateOrUpdateVendorValidator),vendorController.CreateNewVendor);
+vendorRoutes.post("/vendor/create",validate(VendorValidations.CreateOrUpdateVendorValidator),vendorController.CreateNewVendor);
+vendorRoutes.get("/brands",vendorController.GetAllVendorsList);
+vendorRoutes.get("/brand/id",vendorController.GetVendorById);
+vendorRoutes.put("/brand/update",validate(VendorValidations.CreateOrUpdateVendorValidator),vendorController.UpdateVendorbyId);
+vendorRoutes.delete("/brand/delete",vendorController.DeleteVendorById);
 
 export default vendorRoutes;
