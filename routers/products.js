1632e936ea8bdac46e672778dfe3bc96785e63cd
Pujan Gautam <mepujan10@gmail.com>
2022-11-08 15:17:32 -0500

    mend

--- routers/products.js
+++ routers/products.js
@@ -2,12 +2,15 @@ import express from 'express';
 import multer from 'multer';
 import { ProductController } from '../controllers/product.js';
 import { ProductStorage } from '../image_handler/handler.js';
+// import { ProductValidation } from '../Validation/products.js';
+// import {createValidator} from 'express-joi-validation';
+
 
 
 const productRouter = express.Router();
 const productController = new ProductController();
 const uploadProductImage = multer({storage:ProductStorage});
-
+// const validator = createValidator();
 productRouter.post('/product/create',uploadProductImage.single('image'),productController.CreateNewProduct);
 productRouter.get('/products',productController.GetAllProducts);
 productRouter.get('/product/id',productController.GetProductById);
